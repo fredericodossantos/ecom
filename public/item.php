53bf4c7fa61a382ea5652eaa85611e47bf13c072
Frederico <frederico.dos.santos@gmail.com>
2022-07-21 15:51:29 -0300

    Single Product Page Setup 27

--- public/item.php
+++ public/item.php
@@ -10,11 +10,24 @@
 
        <?php include(TEMPLATE_FRONT . DS . "side_nav.php") ?>
 
+       <?php
+
+            $query = query(" SELECT * FROM products WHERE product_id =" . escape_string($_GET['id']). " ");
+            confirm($query);
+
+            while($row = fetch_array($query)):
+
+                
+            
+
+
+        ?>
+
 <div class="col-md-9">
 
-<!--Row For Image and Short Description-->
+    <!--Row For Image and Short Description-->
 
-<div class="row">
+    <div class="row">
 
     <div class="col-md-7">
        <img class="img-responsive" src="https://picsum.photos/320/150" alt="">
@@ -23,15 +36,15 @@
 
     <div class="col-md-5">
 
-        <div class="thumbnail">
+    <div class="thumbnail">
          
 
-    <div class="caption-full">
-        <h4><a href="#">Javascript Course</a> </h4>
+   <div class="caption-full">
+        <h4><a href="#"><?php echo $row['product_title']; ?></a> </h4>
         <hr>
-        <h4 class="">$24.99</h4>
+        <h4 class="">R&#36 <?php echo $row['product_price'];  ?></h4>
 
-    <div class="ratings">
+         <div class="ratings">
      
         <p>
             <span class="glyphicon glyphicon-star"></span>
@@ -41,16 +54,17 @@
             <span class="glyphicon glyphicon-star-empty"></span>
             4.0 stars
         </p>
-    </div>
+        
+        </div>
           
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p><?php echo $row['short_desc'];  ?> </p>
 
    
-    <form action="">
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="ADD TO CART">
-        </div>
-    </form>
+        <form action="">
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="ADD TO CART">
+            </div>
+        </form>
 
     </div>
  
@@ -84,13 +98,9 @@
 
 <p></p>
            
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    <p><?php echo $row['product_description'];  ?></p>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
+    
     </div>
     <div role="tabpanel" class="tab-pane" id="profile">
 
@@ -194,7 +204,9 @@
 
 
 
-</div>
+</div><!-- col-md-9 ens here -->
+
+<?php endwhile; ?>
 
 </div>
     <!-- /.container -->
